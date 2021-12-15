5799c6a43b3978ceecadc586993e6500279cf686
sivaninimmana009 <95833215+sivaninimmana009@users.noreply.github.com>
2021-12-15 02:43:25 -0500

    Create content_management.py

---
+++ content_management.py
@@ -0,0 +1,6 @@
+def Content():
+    TOPIC_DICT = {"Basics":[['Introduction to Python','/introduction-to-python'],
+                            ['Print functions and Strings','/python-functions-and-strings']],
+                'Web Dev':[]}
+
+    return TOPIC_DICT
